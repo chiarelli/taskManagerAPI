fcf3d3949f93401fa8693e8469dd2e8a80e8290d
Raphael Mathias <oi.sucesso@gmail.com>
2025-07-07 15:27:14 -0300

    Created the main project dependencies

---
+++ presentation/src/main/java/com/github/chiarelli/taskmanager/presentation/presentation/PresentationApplication.java
@@ -0,0 +1,13 @@
+package com.github.chiarelli.taskmanager.presentation.presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresentationApplication.class, args);
+	}
+
+}

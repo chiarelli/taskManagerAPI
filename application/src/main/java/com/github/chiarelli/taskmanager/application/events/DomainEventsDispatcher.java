e8796d2f2c0acd9d4c87a925e0fc5358427a0864
Raphael Mathias <oi.sucesso@gmail.com>
2025-07-11 21:07:04 -0300

    Implemented "EventsDispatcher" as "DomainEventsDispatcher"

---
+++ application/src/main/java/com/github/chiarelli/taskmanager/application/events/DomainEventsDispatcher.java
@@ -0,0 +1,44 @@
+package com.github.chiarelli.taskmanager.application.events;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.stereotype.Component;
+
+import com.github.chiarelli.taskmanager.application.shared.EventsDispatcher;
+import com.github.chiarelli.taskmanager.domain.event.AbstractDomainEvent;
+import com.github.chiarelli.taskmanager.domain.model.AbstractModelEvents;
+
+import io.github.jkratz55.mediator.core.Mediator;
+
+@Component
+public class DomainEventsDispatcher implements EventsDispatcher {
+
+  private List<AbstractDomainEvent<?>> events = new ArrayList<>();
+  private final Mediator eventPublisher;
+
+  public DomainEventsDispatcher(Mediator eventPublisher) {
+    this.eventPublisher = eventPublisher;
+  }
+
+  @Override
+  public void collectFrom(AbstractModelEvents... aggregates) {
+    Arrays.stream(aggregates)
+      .filter(Objects::nonNull)
+      .map(AbstractModelEvents::dumpEvents)
+      .flatMap(Collection::stream)
+      .forEach(events::add);
+  }
+
+  @Override
+  public void emitAll() {
+    events.stream()
+      .map(DomainEventsAdapter::adapt)
+      .forEach(eventPublisher::emit);
+    events.clear();
+  }
+
+}

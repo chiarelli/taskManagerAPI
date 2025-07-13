9485a16eb05caf3121baed7aeeb19a9b43a7158c
Raphael Mathias <oi.sucesso@gmail.com>
2025-07-13 02:54:48 -0300

    "NewTaskCreated" event logged

---
+++ application/src/main/java/com/github/chiarelli/taskmanager/application/handlers/logger/TarefaCriadaEventsLogger.java
@@ -0,0 +1,19 @@
+package com.github.chiarelli.taskmanager.application.handlers.logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.github.chiarelli.taskmanager.application.events.DomainEventAdapters.NovaTarefaCriadaEventAdapter;
+import com.github.chiarelli.taskmanager.application.shared.EventHandler;
+
+@Component
+class TarefaCriadaEventsLogger implements EventHandler<NovaTarefaCriadaEventAdapter> {
+
+  private static final Logger logger = LoggerFactory.getLogger(TarefaCriadaEventsLogger.class);
+
+  @Override
+  public void handle(NovaTarefaCriadaEventAdapter event) {
+    logger.info("Tarefa %s criada com sucesso".formatted(event.getAggregate().getIdAsString()));
+  }
+}

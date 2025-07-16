0a597fd80566082dcbd55eaaef86f596893a8007
Raphael Mathias <oi.sucesso@gmail.com>
2025-07-16 16:56:50 -0300

    Changed method signatures of the iProjetoRepository and iTarefasRepository interfaces

--- domain/src/main/java/com/github/chiarelli/taskmanager/domain/repository/iProjetoRepository.java
+++ domain/src/main/java/com/github/chiarelli/taskmanager/domain/repository/iProjetoRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.github.chiarelli.taskmanager.domain.entity.ProjetoId;
+import com.github.chiarelli.taskmanager.domain.entity.TarefaId;
 import com.github.chiarelli.taskmanager.domain.model.Projeto;
 import com.github.chiarelli.taskmanager.domain.model.Tarefa;
 
@@ -11,9 +12,13 @@ public interface iProjetoRepository {
 
   Optional<Projeto> findById(ProjetoId id);
 
+  Optional<Tarefa> findTarefaByProjetoId(ProjetoId projetoId, TarefaId tarefaId);
+
   void save(Projeto projeto);
   
-  void softDelete(Projeto projeto);
+  void delete(Projeto projeto);
+
+  void deleteTarefa(ProjetoId projetoId, TarefaId tarefaId);
 
   List<Projeto> findAllByUserId(String userId);
 
--- domain/src/main/java/com/github/chiarelli/taskmanager/domain/repository/iTarefasRepository.java
+++ domain/src/main/java/com/github/chiarelli/taskmanager/domain/repository/iTarefasRepository.java
@@ -1,23 +1,19 @@
 package com.github.chiarelli.taskmanager.domain.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import com.github.chiarelli.taskmanager.domain.entity.TarefaId;
 import com.github.chiarelli.taskmanager.domain.model.Comentario;
 import com.github.chiarelli.taskmanager.domain.model.Historico;
-import com.github.chiarelli.taskmanager.domain.model.Tarefa;
 
 public interface iTarefasRepository {
 
-  Optional<Tarefa> findById(TarefaId id);
-
-  void save(Tarefa tarefa);
-
-  void delete(Tarefa tarefa);
-
-  List<Historico> findAllHistoricoByTarefaId(TarefaId id);
+  List<Historico> findAllHistoricosByTarefaId(TarefaId id);
 
   List<Comentario> findAllComentariosByTarefaId(TarefaId id);
+
+  void saveHistorico(TarefaId tarefaId, Historico historico);
+
+  void saveComentario(TarefaId tarefaId, Comentario comentario);
   
 }

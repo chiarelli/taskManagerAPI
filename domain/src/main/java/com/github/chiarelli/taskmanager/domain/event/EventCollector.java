f4921adecc416e783334fb6676ca48f2c80f9f61
Raphael Mathias <oi.sucesso@gmail.com>
2025-07-16 16:54:50 -0300

    Deprecated interface

--- domain/src/main/java/com/github/chiarelli/taskmanager/domain/event/EventCollector.java
+++
@@ -1,9 +0,0 @@
-package com.github.chiarelli.taskmanager.domain.event;
-
-import com.github.chiarelli.taskmanager.domain.model.AbstractModelEvents;
-
-public interface EventCollector {
-
-  void collectFrom(AbstractModelEvents... aggregates);
-
-}

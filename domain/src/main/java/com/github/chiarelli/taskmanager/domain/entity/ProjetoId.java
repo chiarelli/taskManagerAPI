05c058c549934afc620e4c18473cab20cf33144c
Raphael Mathias <oi.sucesso@gmail.com>
2025-07-17 23:51:45 -0300

    Constructors declared in the entities "ProjectId" and "TaskId"

--- domain/src/main/java/com/github/chiarelli/taskmanager/domain/entity/ProjetoId.java
+++ domain/src/main/java/com/github/chiarelli/taskmanager/domain/entity/ProjetoId.java
@@ -1,5 +1,15 @@
 package com.github.chiarelli.taskmanager.domain.entity;
 
+import java.util.UUID;
+
 public class ProjetoId extends AbstractUUIDEntity {
 
+  public ProjetoId(UUID id) {
+    super(id);
+  }
+
+  public ProjetoId() {
+    super();
+  }
+  
 }
--- domain/src/main/java/com/github/chiarelli/taskmanager/domain/entity/TarefaId.java
+++ domain/src/main/java/com/github/chiarelli/taskmanager/domain/entity/TarefaId.java
@@ -1,5 +1,15 @@
 package com.github.chiarelli.taskmanager.domain.entity;
 
+import java.util.UUID;
+
 public class TarefaId extends AbstractUUIDEntity {
+
+  public TarefaId(UUID id) {
+    super(id);
+  }
+
+  public TarefaId() {
+    super();
+  }
 
 }

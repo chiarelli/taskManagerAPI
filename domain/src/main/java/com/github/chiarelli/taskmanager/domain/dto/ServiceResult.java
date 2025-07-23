7147d4b6d69636c9450d07edc4324728feea3882
Raphael Mathias <oi.sucesso@gmail.com>
2025-07-23 17:21:31 -0300

    Artifact "ServiceResult" is now also of type "iEventFlusher"

--- domain/src/main/java/com/github/chiarelli/taskmanager/domain/dto/ServiceResult.java
+++ domain/src/main/java/com/github/chiarelli/taskmanager/domain/dto/ServiceResult.java
@@ -3,8 +3,26 @@ package com.github.chiarelli.taskmanager.domain.dto;
 import java.util.List;
 
 import com.github.chiarelli.taskmanager.domain.event.AbstractDomainEvent;
+import com.github.chiarelli.taskmanager.domain.shared.iEventFlusher;
 
-public record ServiceResult<T>(
-  T result,
-  List<AbstractDomainEvent<?>> events
-) { }
+public class ServiceResult<T> implements iEventFlusher {
+  private T result;
+  private List<AbstractDomainEvent<?>> events;
+
+  public ServiceResult(T result, List<AbstractDomainEvent<?>> events) {
+    this.result = result;
+    this.events = events;
+  }
+
+  @Override
+  public List<AbstractDomainEvent<?>> flushEvents() {
+    List<AbstractDomainEvent<?>> result = List.copyOf(events);
+    events.clear();
+    return result;
+  }
+  
+  public T result() {
+    return result;
+  }
+  
+}

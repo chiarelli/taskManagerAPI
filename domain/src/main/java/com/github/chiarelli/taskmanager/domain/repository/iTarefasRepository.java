47acc2baf1a9ebba18747d450d7c3321686a63fd
Raphael Mathias <oi.sucesso@gmail.com>
2025-07-23 00:09:22 -0300

    Added method to retrieve a comment by id in the "iTarefasRepository" interface

--- domain/src/main/java/com/github/chiarelli/taskmanager/domain/repository/iTarefasRepository.java
+++ domain/src/main/java/com/github/chiarelli/taskmanager/domain/repository/iTarefasRepository.java
@@ -1,7 +1,9 @@
 package com.github.chiarelli.taskmanager.domain.repository;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.github.chiarelli.taskmanager.domain.entity.ComentarioId;
 import com.github.chiarelli.taskmanager.domain.entity.TarefaId;
 import com.github.chiarelli.taskmanager.domain.model.Comentario;
 import com.github.chiarelli.taskmanager.domain.model.Historico;
@@ -11,6 +13,8 @@ public interface iTarefasRepository {
   List<Historico> findAllHistoricosByTarefaId(TarefaId id);
 
   List<Comentario> findAllComentariosByTarefaId(TarefaId id);
+
+  Optional<Comentario> findComentarioByComentarioIdAndTarefaId(TarefaId id, ComentarioId comentarioId);
 
   void saveHistorico(TarefaId tarefaId, Historico historico);
 

4c408c6fe372233e94ecaa9f562c1a88f92cbc1e
Raphael Mathias <oi.sucesso@gmail.com>
2025-07-25 04:11:54 -0300

    Added constructors

--- domain/src/main/java/com/github/chiarelli/taskmanager/domain/entity/ComentarioId.java
+++ domain/src/main/java/com/github/chiarelli/taskmanager/domain/entity/ComentarioId.java
@@ -1,5 +1,15 @@
 package com.github.chiarelli.taskmanager.domain.entity;
 
+import java.util.UUID;
+
 public class ComentarioId extends AbstractUUIDEntity {
+
+  public ComentarioId(UUID id) {
+    super(id);
+  }
+
+  public ComentarioId() {
+    super();
+  }
 
 }
--- domain/src/main/java/com/github/chiarelli/taskmanager/domain/entity/HistoricoId.java
+++ domain/src/main/java/com/github/chiarelli/taskmanager/domain/entity/HistoricoId.java
@@ -1,5 +1,15 @@
 package com.github.chiarelli.taskmanager.domain.entity;
 
+import java.util.UUID;
+
 public class HistoricoId extends AbstractUUIDEntity {
+
+  public HistoricoId(UUID id) {
+    super(id);
+  }
+
+  public HistoricoId() {
+    super();
+  }
 
 }

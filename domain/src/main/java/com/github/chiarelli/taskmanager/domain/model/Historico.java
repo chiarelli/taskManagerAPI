c515f269df37998db00da3812be4037bb19f9496
Raphael Mathias <oi.sucesso@gmail.com>
2025-08-19 22:34:13 -0300

    Added lombok package notes to the "Historico" model

--- domain/src/main/java/com/github/chiarelli/taskmanager/domain/model/Historico.java
+++ domain/src/main/java/com/github/chiarelli/taskmanager/domain/model/Historico.java
@@ -5,14 +5,20 @@ import java.util.Date;
 import com.github.chiarelli.taskmanager.domain.entity.AutorId;
 import com.github.chiarelli.taskmanager.domain.entity.HistoricoId;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 @RequiredArgsConstructor
 @Getter
+@ToString
+@EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
 public class Historico {
 
+  @EqualsAndHashCode.Include
   private final HistoricoId id;
+  
   private final Date dataOcorrencia;
   private final String titulo;
   private final String descricao;

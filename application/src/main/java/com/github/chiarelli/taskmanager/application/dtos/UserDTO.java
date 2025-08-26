447f29686c4bd6edfafff6e51b1d55374a08064c
Raphael Mathias <oi.sucesso@gmail.com>
2025-08-26 01:33:05 -0300

    Changing the name of DTO attributes that represent a logged-in user

--- application/src/main/java/com/github/chiarelli/taskmanager/application/dtos/UserDTO.java
+++ application/src/main/java/com/github/chiarelli/taskmanager/application/dtos/UserDTO.java
@@ -2,18 +2,19 @@ package com.github.chiarelli.taskmanager.application.dtos;
 
 import java.util.UUID;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
-@Data
+@Getter
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
 public class UserDTO {
-
   @EqualsAndHashCode.Include
   private UUID id;
   
-  private String nome;
+  private String username;
   private String email;
-  private String papel;
+  private String role;
   
 }
